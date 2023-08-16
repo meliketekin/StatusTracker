9c1a2c74b7bcb1df7ecc9712bcf3c1563a064c6c
Melike <meliketekin2243@gmail.com>
2023-08-16 20:19:31 +0300

    feat: folder structure setup

---
+++ screens/EditUserScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function EditUserScreen() {
+  return (
+    <View>
+      <Text>EditUserScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
